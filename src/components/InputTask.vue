6e42d27bc5a341a44dd4d67fb9bf8c71a1f39350
Leandro <leandro.ragni@gmail.com>
2017-11-14 01:20:48 +0000

    projeto vue.js

---
+++ src/components/InputTask.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>
+      <input class="new_todo" @keyup.enter="addTask" placeholder="O que precisa ser feito" />
+    </p>
+  </div>
+</template>
+
+<script>
+import { Task } from '../models/Task'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    addTask ($event) {
+      let value = $event.target.value
+      let task = new Task()
+      task.completed = false
+      task.title = value
+      this.$emit('newTask', task)
+      event.target.value = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,30 @@
+<template>
+	<ul	class="todo-list">
+		<li	v-for="todo	in sortedTasks" class="todo">
+			<div class="view">
+				<label>{{	todo.title	}}</label>
+			</div>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+  props: ['todoList'],
+  computed: {
+    sortedTasks: function () {
+      console.log('que merda')
+      let sorted = this.todoList
+      return sorted.sort(function (a, b) {
+        console.log('aqui')
+        if (a.title < b.title) return -1
+        if (a.title > b.title) return 1
+        return 0
+      })
+    }
+  },
+  set: function (novalista) {
+    this.todoList.concat(novalista)
+  }
+}
+</script>
