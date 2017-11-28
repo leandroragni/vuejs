fc54e437dfd3819f287b614043a0eaa2905f7824
Leandro <leandro.ragni@gmail.com>
2017-11-28 01:18:11 +0000

    consumindo api

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const router = new VueRouter({
   mode: 'history',
